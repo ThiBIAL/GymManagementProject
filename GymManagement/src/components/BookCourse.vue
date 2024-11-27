1e0a532544ddb92d104c0513003eb35722268c73
ThiBIAL <tibobial@gmail.com>
2024-11-27 09:55:14 +0800

    test

--- GymManagement/src/components/BookCourse.vue
+++ GymManagement/src/components/BookCourse.vue
@@ -11,27 +11,27 @@
         <p><strong>Time:</strong> {{ course.time }}</p>
         <button @click="bookCourse(course)">Book Now</button>
       </div>
-    </div>
 
-    <div v-if="selectedCourse" class="confirmation">
-      <h3>Confirmation</h3>
-      <p>You have selected: <strong>{{ selectedCourse.name }}</strong></p>
-      <p><strong>Coach:</strong> {{ selectedCourse.coach }}</p>
-      <p><strong>Day:</strong> {{ selectedCourse.day }}</p>
-      <p><strong>Time:</strong> {{ selectedCourse.time }}</p>
-      <button @click="confirmBooking">Confirm Booking</button>
+      <div v-if="selectedCourse" class="course confirmation">
+        <h3>Confirmation</h3>
+        <p>You have selected: <strong>{{ selectedCourse.name }}</strong></p>
+        <p><strong>Coach:</strong> {{ selectedCourse.coach }}</p>
+        <p><strong>Day:</strong> {{ selectedCourse.day }}</p>
+        <p><strong>Time:</strong> {{ selectedCourse.time }}</p>
+        <button @click="confirmBooking">Confirm Booking</button>
+      </div>
     </div>
   </div>
 </template>
-  
+
 <script>
   export default {
     data() {
       return {
         courses: [
-          { id: 1, name: "Yoga Class", coach:"Samuel Doyen", day: "Monday", time: "4:00 PM" },
-          { id: 2, name: "Pilates Class", coach:"Thibault Bial", day: "Wednesday", time: "10:00 AM" },
-          { id: 3, name: "Zumba Class", coach:"Ethan Guingand", day: "Friday", time: "2:00 PM" },
+          { id: 1, name: "Yoga Class", coach: "Samuel Doyen", day: "Monday", time: "4:00 PM" },
+          { id: 2, name: "Pilates Class", coach: "Thibault Bial", day: "Wednesday", time: "10:00 AM" },
+          { id: 3, name: "Zumba Class", coach: "Ethan Guingand", day: "Friday", time: "2:00 PM" },
         ],
         selectedCourse: null,
       };
@@ -47,7 +47,7 @@
     },
   };
 </script>
-  
+
 <style scoped>
   .book-course {
     font-family: Arial, sans-serif;
@@ -67,7 +67,7 @@
     padding: 10px;
     border-radius: 8px;
     width: 80%;
-    box-sizing: border-box; 
+    box-sizing: border-box;
   }
   .course > * {
     margin: 10px;
@@ -81,20 +81,19 @@
     border-radius: 8px;
     cursor: pointer;
     transition: background-color 0.3s ease;
-    margin: 10px
+    margin: 10px;
   }
   button:hover {
     background-color: #FF8C00;
   }
   .confirmation {
-    margin-top: 20px;
     padding: 15px;
     border: 1px solid #007bff;
-    border-radius: 5px;
+    border-radius: 8px;
     background-color: #f0f8ff;
+    width: 80%;
   }
   .description {
     margin: 15px;
   }
 </style>
-  
